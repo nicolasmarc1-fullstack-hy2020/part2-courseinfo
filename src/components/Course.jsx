7d3f4cb4e3bc5c00fd795bf062f7d67765ba8e94
nico <nicolas.marcg@gmail.com>
2020-05-22 21:04:35 +0200

    submit

---
+++ src/components/Course.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const Header = ({ name }) => {
+
+  return (
+    <h1>{name}</h1>
+  )
+}
+
+
+const Content = ({ parts }) => {
+  const sum = parts.reduce((accu, curval) => accu + curval.exercises, 0)
+  return (
+    <>
+      {parts.map((part) =>
+        <Part key={part.id} part={part} />
+      )}
+      <Total sum={sum} />
+    </>
+  )
+}
+const Part = ({ part }) => {
+  return (
+    <p>{part.name} : {part.exercises}</p>
+  )
+}
+
+
+const Total = ({ sum }) => {
+  return (
+    <p><strong>total of {sum} exercises</strong></p>
+  )
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  )
+}
+
+export default Course
